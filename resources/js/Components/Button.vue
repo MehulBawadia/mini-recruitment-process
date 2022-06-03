8f9c2391e4d9057dd33789b74c9877647d0dbde1
Mehul Bawadia <mehulbawadia@gmail.com>
2022-06-03 18:28:04 +0530

    Minor UI changes in components

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -4,11 +4,15 @@ defineProps({
         type: String,
         default: 'submit',
     },
+    processing: {
+        type: Boolean,
+        default: false,
+    }
 });
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+    <button :type="type" class="font-bold inline-flex items-center px-4 py-2 bg-indigo-800 border border-transparent rounded-md text-sm text-white tracking-widest hover:bg-amber-600 focus:bg-amber-600 focus:outline-none transition ease-in-out duration-150" :processing="processing">
         <slot />
     </button>
 </template>
